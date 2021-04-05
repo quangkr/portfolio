cdf3dd0e6119fc850f992e3b9dcb3eeb96094ab9
QuangKR <dangminhquang.r@gmail.com>
2021-04-05 23:47:40 +0700

    Add Home layout; use Home layout for index page

---
+++ docs/.vuepress/theme/layouts/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page">
+    <section id="section-hero" class="section-hero">
+      <div class="hero-inner">
+        <h1 class="hero-title">{{ frontmatter.hero.title }}</h1>
+        <h2 class="hero-description">{{ frontmatter.hero.description }}</h2>
+        <a href="#section-projects" target="_self" class="cta-link">
+          {{ frontmatter.hero["cta-text"] }}
+        </a>
+      </div>
+    </section>
+    <section id="section-projects" class="section-projects"></section>
+    <Content />
+    <footer>{{ frontmatter.footer }}</footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { usePageFrontmatter } from "@vuepress/client";
+export default defineComponent({
+  name: "Home",
+
+  setup() {
+    const frontmatter = usePageFrontmatter();
+
+    return {
+      frontmatter,
+    };
+  },
+});
+</script>
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,8 @@
-# Hello VuePress
+---
+layout: Home
+hero:
+  title: Hi, I am Quang
+  description: I am a frontend developer
+  cta-text: See my works
+footer: Copyright © 2021 Dang Minh Quang
+---
