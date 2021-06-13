4cb51b829f69317cd505b09877d272e7a8002130
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-06-13 17:19:11 +0700

    Add basic About section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,7 +16,7 @@ const router = useRouter()
     >
       <div class="xl:container mx-auto px-4 grid grid-cols-1 grid-rows-3 min-h-screen">
         <SvgLogo class="w-full max-w-60 h-auto mx-auto px-8 row-start-1 self-end" />
-        <h1 class="row-start-2 self-center font-sans text-3xl text-console-8">
+        <h1 class="row-start-2 self-center text-3xl text-console-8">
           Hi, I'm <span class="text-console-2">Quang</span>
           <p class="mt-6 font-normal text-2xl">
             I'm a <span class="text-console-3">Web Developer</span>
@@ -25,6 +25,17 @@ const router = useRouter()
         <a href="#section-projects" target="_self" class="row-start-3 self-end text-console-8">
           <mdi-light-chevron-down class="w-28 h-auto" />
         </a>
+      </div>
+    </section>
+    <section id="section-about" class="w-full">
+      <div class="mx-auto max-w-screen-lg px-8 py-24">
+        <h2 class="mb-12 text-console-7 text-3xl text-center">
+          About me
+        </h2>
+        <p class="text-console-7 text-lg">
+          I'm a frontend web developer.
+          I create modern and clean websites using tools and frameworks like React, Vue and Tailwind.
+        </p>
       </div>
     </section>
     <section id="section-projects" class="section-projects"></section>
