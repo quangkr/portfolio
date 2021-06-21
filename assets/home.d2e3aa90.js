1022faa43167f7837e2c81c07ae3a4b19054858b
quangkr <49685785+quangkr@users.noreply.github.com>
2021-06-21 03:53:06 +0000

    Deploying to gh-pages from @ quangkr/portfolio@6a645750a4c32e73d402f506b908a5a4859d4ecf 🚀

--- assets/home.d2e3aa90.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app.00e2405a.js";import{j as a,o as e,a as r,b as o}from"./vendor.6095adcd.js";const s={},n={class:"px-4 py-10 text-center text-gray-700 dark:text-gray-200"},c=o("div",{class:"mt-5 mx-auto text-center opacity-25 text-sm"}," [Home Layout] ",-1);s.render=function(s,m){const p=a("router-view"),x=t;return e(),r("main",n,[o(p),o(x),c])};export default s;
