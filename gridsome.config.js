2c5c91291374f87f9873120a9cdeed74b4a2e165
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-06-22 18:46:09 +0700

    Add Netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,40 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Portfolio',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/blogs/**/*.md',
+        typeName: 'BlogPost',
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/projects/**/*.md',
+        typeName: 'ProjectPost',
+      }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin',
+        configPath: 'src/cms/config.yml',
+        htmlTitle: 'Netlify CMS'
+      }
+    },
+  ],
+  templates: {
+    BlogPost: '/blog/:year/:month/:day/:title',
+  }
 }
