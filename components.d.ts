2dd704275232d2c3d603d11c87328eb515c02aa6
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-06-27 12:32:29 +0700

    Remove unnecessary README files

--- components.d.ts
+++ components.d.ts
@@ -4,7 +4,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     Footer: typeof import('./src/components/Footer.vue')['default']
-    README: typeof import('./src/components/README.md')['default']
   }
 }
 
