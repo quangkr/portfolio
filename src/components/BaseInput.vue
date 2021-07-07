3ce15fda88e5a68ae8f9a05bcfe939cab6535449
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-07-08 00:18:11 +0700

    Add BaseInput component, add simple contact section

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps, defineEmit } from 'vue'
+
+defineProps({
+  modelValue: String,
+})
+defineEmit(['update:modelValue'])
+</script>
+
+<template>
+  <input
+    class="rounded-md shadow border"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,60 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import axios from 'axios'
 
 import imgThumbBlogr from '~/assets/thumb-blogr.png'
 import imgThumbRestCountries from '~/assets/thumb-rest-countries.png'
 import SvgLogo from '~/assets/logo.svg'
 
 const router = useRouter()
+
+// helpers
+function encode(data: Record<string, string>): string {
+  return Object.keys(data)
+    .map(
+      key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`,
+    )
+    .join('&')
+}
+
+// form handling
+type formDataType = Record<string, string>
+const initialFormData = (): formDataType => ({
+  name: '',
+  email: '',
+  message: '',
+})
+const formData: formDataType = reactive(initialFormData())
+function resetForm(): void {
+  const s: formDataType = initialFormData()
+  Object.keys(s).forEach((key) => {
+    formData[key] = s[key]
+  })
+}
+
+function handleSubmit() {
+  // const axiosConfig = {
+  //   headers: {
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //   },
+  // }
+
+  // axios.post(
+  //   '/',
+  //   encode({
+  //     'form-name': 'contact',
+  //     ...formData,
+  //   }),
+  //   axiosConfig,
+  // )
+
+  console.log(encode({
+    'form-name': 'contact',
+    ...formData,
+  }))
+  resetForm()
+}
 </script>
 
 <template>
@@ -40,13 +89,13 @@ const router = useRouter()
         </p>
       </div>
     </section>
-    <section id="section-projects" class="section-projects">
+    <section id="section-projects" class="w-full">
       <feature-card
         class="max-w-160 w-8/10 mx-auto my-12"
         :img-src="imgThumbBlogr"
         title="Blogr"
       >
-        A very simple landing page with no interactivity and no routes. I used Vue with simple HTML and CSS to make this site.
+        A very simple landing page with no interactivity and no routes; written in Vue with simple HTML and CSS.
       </feature-card>
       <feature-card
         class="max-w-160 w-8/10 mx-auto my-12"
@@ -56,6 +105,33 @@ const router = useRouter()
         A simple SPA that fetches and displays data from <a href="https://restcountries.eu/" target="_blank">REST Countries API</a>.
         Written in React, it features a simple filter and a search bar, and also a Dark mode toggle.
       </feature-card>
+    </section>
+    <section id="section-contact" class="w-full">
+      <div class="mx-auto max-w-screen-lg px-8 py-24">
+        <h2 class="mb-12 text-console-7 text-3xl text-center">
+          Let's chat
+        </h2>
+        <div class="w-full">
+          <form
+            class="w-full grid grid-cols-[max-content,1fr] gap-x-4 gap-y-6"
+            name="contact"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            @submit.prevent="handleSubmit"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <label for="formdata-name">Name</label>
+            <base-input id="formdata-name" v-model="formData.name" type="text" name="name" />
+            <label for="formdata-email">Email</label>
+            <base-input id="formdata-email" v-model="formData.email" type="email" name="email" />
+            <label for="formdata-message">Message</label>
+            <base-input id="formdata-message" v-model="formData.message" type="textarea" name="message" />
+            <base-button type="submit">
+              Send
+            </base-button>
+          </form>
+        </div>
+      </div>
     </section>
   </div>
 </template>
